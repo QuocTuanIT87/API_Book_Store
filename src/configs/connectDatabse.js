e108c653124514074d774f0b363a719df58edb37
QuocTuanIT87 <121918629+QuocTuanIT87@users.noreply.github.com>
2023-10-25 15:43:20 +0700

    Update connectDatabse.js

--- src/configs/connectDatabse.js
+++ src/configs/connectDatabse.js
@@ -6,6 +6,8 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   database: process.env.DB_DBNAME,
+    password: process.env.DB_PASSWORD
+
 });
 
 export default pool;
