bcc458f5998b9b1469f984c7da544eb71447d144
Tuan <quoctuanit87@gmail.com>
2023-10-25 15:53:49 +0700

    add

--- src/configs/connectDatabse.js
+++ src/configs/connectDatabse.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   database: process.env.DB_DBNAME,
+  password: process.env.DB_PASSWORD
 });
 
 export default pool;
